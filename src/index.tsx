0a5c885c53838290a66cd7c2755eecf4ed24787c
Oren Shamir <orenshamir5@gmail.com>
2026-01-31 11:21:34 +0200

    FORCE CACHE BUST v27: Renamed CSS, Added Marker Banner

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index-v27.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
